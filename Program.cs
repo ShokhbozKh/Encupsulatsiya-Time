7ecb463ab67608b74cd37a3c2dde9eed97eb152f
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-22 13:08:28 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hours = DateTime.Now.Hour;
+            var minutes = DateTime.Now.Minute;
+            var seconds = DateTime.Now.Second;
+
+            Time time = new Time();
+            time.Soat = hours;
+            time.Minut = minutes;
+            time.Sekund = seconds;
+
+            time.DesplayInfo();
+            
+        }
+    }
+}
---
+++ Times.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Time
+    {
+        //Quyidagi 3ta field'larga ega bo’lgan
+        //Time klassini encapsulation prinsipi asosida yarating: soat, minut, va sekund.
+        //“soat:minut:sekund” (Masalan: 01:25:08)
+        //formatdagi vaqtni qaytaruvchi metod yarating.
+        private int _soat;
+        private int _minut;
+        private int _sekund;
+
+        public int Soat
+        {
+            get { return _soat; }
+            set 
+            { 
+                if(value >= 0 && value <= 25)
+                _soat = value;
+                else
+                {
+                    Console.WriteLine("soatda xatolik bor");
+                }
+            }
+        }
+        public int Minut
+        {
+            get { return _minut; }
+            set 
+            {
+                if (value >= 0 && value <= 60)
+                    _minut = value;
+                else
+                {
+                    Console.WriteLine("minutda xatolik bor");
+                }
+                 
+            }
+        }
+        public int Sekund
+        {
+            get { return _sekund; }
+            set 
+            {
+                if (value >= 0 && value <= 60)
+                    _sekund = value;
+                else
+                {
+                    Console.WriteLine("secundda xatolik bor");
+                }
+                 
+            }
+        }
+        public void DesplayInfo()
+        {
+            Console.WriteLine($"{_soat}:{_minut}:{_sekund}");
+        }
+
+    }
+}
